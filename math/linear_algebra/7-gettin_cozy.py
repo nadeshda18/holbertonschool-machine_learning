542c2bf09a8ad335a2a67fd207603a83f6d8145f
nadeshda18 <nadeshda02@hotmail.com>
2024-04-11 02:46:24 -0400

    gettin cozy/concat two matrices along a specific axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""concatenate two matrices
+along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenate two matrices
+    along a specific axis
+    Args:
+        mat1: first matrix
+        mat2: second matrix
+        axis: axis to concatenate
+    Returns:
+        concatenated matrix"""
+    """checks the number of rows"""
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return [row.copy() for row in mat1] + [row.copy() for row in mat2]
+    """checks the number of columns"""
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [mat1[i].copy() + mat2[i].copy() for i in range(len(mat1))]
