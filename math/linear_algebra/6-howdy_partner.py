3837b5774b1ca000b4d3e623eb1badeba436c8cf
nadeshda18 <nadeshda02@hotmail.com>
2024-04-11 02:39:53 -0400

    howdy partner/concatenate two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""concatenate two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """concatenate two arrays
+    Args:
+        arr1: first array
+        arr2: second array
+    Returns:
+        concatenated array"""
+    return arr1 + arr2
