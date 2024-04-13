c1784263d38d03bfccebf191f15a7ba9b8180331
nadeshda18 <nadeshda02@hotmail.com>
2024-04-13 00:02:38 -0400

    saddle up/matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """matrix multiplication
+    Args:
+        mat1: numpy.ndarray
+        mat2: numpy.ndarray
+    Returns: new matrix"""
+    return np.matmul(mat1, mat2)
