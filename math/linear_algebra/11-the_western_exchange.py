0c86b2b94189bb7a93d07fb58dcdd584e393e361
nadeshda18 <nadeshda02@hotmail.com>
2024-04-12 23:19:44 -0400

    The western exchange/transpose a matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""transpose a matrix"""
+
+
+def np_transpose(matrix):
+    """transpose a matrix
+    Args:
+        matrix: numpy.ndarray
+    Returns: new matrix"""
+    """start at 0, and flip it"""
+    return matrix.T
