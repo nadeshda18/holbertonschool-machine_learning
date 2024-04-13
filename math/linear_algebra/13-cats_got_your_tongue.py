da32f6f16418ffa68a0fa94ecd1c4e1506c79804
nadeshda18 <nadeshda02@hotmail.com>
2024-04-12 23:40:29 -0400

    Cat's got your tongue/concatenate two matricess along a specific axis

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+"""concatenates two matrices
+along a specific axis"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """concatenates two matrices
+    along a specific axis
+    Args:
+        mat1: numpy.ndarray
+        mat2: numpy.ndarray
+        axis: axis to concatenate
+    Returns:
+        concatenated matrix"""
+    """for axis=0, along the rows, stacked one
+    on top of the other/number of columns must
+    be the same"""
+    """for axis=1, along the columns, stacked side
+    by side/number of rows must be the same"""
+    return np.concatenate((mat1, mat2), axis=axis)
