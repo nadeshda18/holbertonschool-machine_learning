56ad4d2f898b799a346ef87e3a60d21deaded31b
nadeshda18 <nadeshda02@hotmail.com>
2024-04-12 23:13:46 -0400

    I'll use my scale/calculate the shape of a numpy.ndarray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""calcaulte the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """calculate the shape of a numpy.ndarray
+    Args:
+        matrix: numpy.ndarray
+    Returns: shape of matrix"""
+    """output 2,2,5==> 2 rows, 2 columns,
+    5 elements in each column"""
+    return matrix.shape
