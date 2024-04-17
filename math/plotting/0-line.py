6fdd2672ef40f13e65a7aec313c64b39cb9b3031
nadeshda18 <nadeshda02@hotmail.com>
2024-04-17 02:03:25 -0400

    documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-
+"""plot y as a line graph"""
 
 def line():
-
+    """y as a red line"""
+    """x should range from 0 - 10"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
