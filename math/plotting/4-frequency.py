f80488dd2f2345590e57e69df8d26f45f1e13ad6
nadeshda18 <nadeshda02@hotmail.com>
2024-04-17 03:02:30 -0400

    wrong output

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,6 +16,7 @@ def frequency():
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
     plt.xlim(0, 100)
     plt.ylim(0, 30)
     plt.show()
