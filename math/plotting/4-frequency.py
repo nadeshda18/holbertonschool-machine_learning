31536ab6ca2ca0cee6c9cbea7402529bdedb1ddd
nadeshda18 <nadeshda02@hotmail.com>
2024-04-17 02:59:54 -0400

    Frequency/plot  a histogram of student scores

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""plot a histogram of student
+scores for a project"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """np.random.seed(5) random number generator
+    student_grades = random"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.show()
