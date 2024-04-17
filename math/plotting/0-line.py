4e5a5c3bd0a6c11fcea3fd567d850fafb810c189
nadeshda18 <nadeshda02@hotmail.com>
2024-04-17 01:58:07 -0400

    line graph/plot y as a line graph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, color='red')
+    plt.show()
