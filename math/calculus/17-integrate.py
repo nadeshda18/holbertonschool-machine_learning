eef32d2a5e4e006dc175c19677d56883cacd8fd2
nadeshda18 <nadeshda02@hotmail.com>
2024-04-21 19:53:23 -0400

    added documentation, adjusted function for edge cases

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,6 +4,13 @@ of a polynomial"""
 
 
 def poly_integral(poly, C=0):
+    """poly: list of coefficients representing a polynomial
+    C: int representing the integration constant
+    Returns: list of coefficients representing the integral of the polynomial
+    Edge Cases:
+    - if poly or C are not valid, return None
+    - if the result is an integer, return it as an integer
+    -the returned list should be as small as possible"""
     if not isinstance(poly, list) or not poly or \
        not all(isinstance(i, (int, float)) for i in poly) or \
        not isinstance(C, (int, float)):
