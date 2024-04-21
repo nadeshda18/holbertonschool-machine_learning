2b23c6a5042ac391f55ba1d8236023c240c024fe
nadeshda18 <nadeshda02@hotmail.com>
2024-04-21 18:47:54 -0400

    integrate

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,14 +7,14 @@ def poly_integral(poly, C=0):
     """poly: a list of coefficients representing a polynomial
     C: an integer representing the integration constant
     Returns: a new list of coefficients"""
-    if not isinstance(poly, list) or \
-       not all(isinstance(i, (int, float)) for i in poly):
-        return None
     if len(poly) == 1:
-        if poly[0] == 0:
-            return [C]
+        if isinstance(poly[0], (int, float)):
+            if poly[0] == 0:
+                return [C]
+            else:
+                return [C, poly[0]]
         else:
-            return [C, poly[0]]
+            return None
     result = [C]
     for i in range(len(poly)):
         coeff = poly[i] / (i + 1)
