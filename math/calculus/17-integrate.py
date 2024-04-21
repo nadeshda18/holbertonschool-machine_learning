211c3e3d81a4468f69a55ecd4e3b133e2b44459f
nadeshda18 <nadeshda02@hotmail.com>
2024-04-21 19:49:47 -0400

    that edge case

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,24 +4,14 @@ of a polynomial"""
 
 
 def poly_integral(poly, C=0):
-    """
-    poly: a list of coefficients representing a polynomial
-    C: an integer representing the integration constant
-    Returns: a new list of coefficients
-    """
     if not isinstance(poly, list) or not poly or \
-       not all(isinstance(i, (int, float)) for i in poly):
+       not all(isinstance(i, (int, float)) for i in poly) or \
+       not isinstance(C, (int, float)):
         return None
-    if len(poly) == 1:
-        if isinstance(poly[0], (int, float)):
-            if poly[0] == 0:
-                return [C]
-            else:
-                return [C, poly[0]]
-        else:
-            return None
     result = [C]
     for i in range(len(poly)):
         coeff = poly[i] / (i + 1)
         result.append(int(coeff) if coeff.is_integer() else coeff)
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
     return result
