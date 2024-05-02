ca3467bac8f123e70b56cb8e9ce8f204bb7a2c21
nadeshda18 <nadeshda02@hotmail.com>
2024-05-02 00:21:10 -0400

    class neural network

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""defines a neural network with one hidden
+layer performing binary classification"""
+import numpy as np
+
+
+class NeuralNetwork:
+    def __init__(self, nx, nodes):
+        """nx = the number of input features to the neuron
+        nodes = the number of nodes found in the hidden layer
+        W1: The weights vector for the hidden layer. Upon instantiation,
+        it should be initialized using a random normal distribution.
+        b1: The bias for the hidden layer. Upon instantiation,
+        it should be initialized with 0’s.
+        A1: The activated output for the hidden layer. Upon instantiation,
+        it should be initialized to 0.
+        W2: The weights vector for the output neuron. Upon instantiation,
+        it should be initialized using a random normal distribution.
+        b2: The bias for the output neuron. Upon instantiation,
+        it should be initialized to 0.
+        A2: The activated output for the output neuron (prediction).
+        Upon instantiation, it should be initialized to 0."""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
