c159db0b4779d1800b53b2170d9c324564192a28
nadeshda18 <nadeshda02@hotmail.com>
2024-05-10 16:47:59 -0400

    wrong import

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """returns two placeholders, x and y, for the neural network"""
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def create_placeholders(nx, classes):
@@ -9,4 +10,5 @@ def create_placeholders(nx, classes):
     classes = number of classes in our classifier"""
     x = tf.placeholder("float", [None, nx], name="x")
     y = tf.placeholder("float", [None, classes], name="y")
+
     return x, y
