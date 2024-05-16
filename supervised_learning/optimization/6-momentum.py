dc0adf71d70d52688dbd4fbe0ce7b349f68fdebe
nadeshda18 <nadeshda02@hotmail.com>
2024-05-16 18:24:57 -0400

    momentum

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""sets up the gradient descent with momentum
+optimization algorithm"""
+import numpy as np
+import tensorflow as tf
+
+def create_momentum_op(alpha, beta1):
+    """alpha = learning rate
+    beta1 = momentum weight
+    Returns: the momentum optimization operation"""
+    optimizer = tf.keras.optimizers.SGD(
+        learning_rate=alpha,
+        momentum=beta1)
+
+    return optimizer
