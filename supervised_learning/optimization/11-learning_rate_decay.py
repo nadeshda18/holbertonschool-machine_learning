7083b5363dee01316dc6c68b63d868308d586cf7
nadeshda18 <nadeshda02@hotmail.com>
2024-05-16 19:11:14 -0400

    pycodestyle

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -13,4 +13,4 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     that should occur before alpha is decayed further
     Returns: the updated value for alpha"""
     alpha = alpha / (1 + decay_rate * np.floor(global_step / decay_step))
-    return(alpha)
+    return (alpha)
